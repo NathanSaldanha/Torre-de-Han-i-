abbc885f1e0a63b997a24133647487b494137dda
unknown <nmariano85@gmail.com>
2020-04-10 18:42:56 -0300

    torre hanoi em java

--- torre-hanoi.java
+++ torre-hanoi.java
@@ -6,13 +6,16 @@ public class Main {
     public static void metodo(int num){
         int base = 2;
         double result = Math.pow(base, num)-1;
+        
         System.out.println("towerHanoi("+num+") => "+(int)result);
     }
 
     public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
+
 		System.out.print("Defina o número de discos: ");
 		int discos = sc.nextInt();
+
         metodo(discos);
     }
 }
